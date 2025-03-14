eb9007409a1e2158554427c3fd4033b134c1ba2f
Jonas Lima <133159361+jonasvlima@users.noreply.github.com>
2025-03-14 04:28:11 -0300

    Initial commit

---
+++ src/main/java/io/github/jonasvlima/arquiteturaspring/ArquiteturaspringApplication.java
@@ -0,0 +1,13 @@
+package io.github.jonasvlima.arquiteturaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturaspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jonasvlima/arquiteturaspring/ArquiteturaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jonasvlima.arquiteturaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
